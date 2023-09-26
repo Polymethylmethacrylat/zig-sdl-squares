6d5738fb3491a0397a8b072d1d95ce398b8198ea
m349pmma <noah.vu.su@gmail.com>
2023-09-26 23:07:43 +0200

    adds a quit-hotkey on <C-q>, fields for bg and fg_color

--- src/SdlApp.zig
+++ src/SdlApp.zig
@@ -5,12 +5,20 @@ const Self = @This();
 
 window: sdl.Window,
 renderer: sdl.Renderer,
+bg_color: sdl.Color = sdl.Color.black,
+fg_color: sdl.Color = sdl.Color.white,
 
 pub fn run(self: *const Self) !void {
     main_loop: while (true) {
         while (sdl.pollEvent()) |e| {
             switch (e) {
                 .window => try self.render(),
+                .key_up => |key_event| {
+                    var is_exit_key = key_event.keycode == .q;
+                    is_exit_key = is_exit_key and key_event.modifiers.get(.left_control);
+                    if (is_exit_key) 
+                        break :main_loop;
+                },
                 .quit => break :main_loop,
                 else => {},
             }
@@ -20,7 +28,7 @@ pub fn run(self: *const Self) !void {
 
 fn render(self: *const Self) !void {
     try self.renderer.clear();
-    try self.renderer.setColorRGB(0, 0, 0);
+    try self.renderer.setColor(self.bg_color);
     try self.renderer.fillRect(self.renderer.getViewport());
     self.renderer.present();
 }
@@ -51,10 +59,13 @@ pub fn init() !Self {
     self.renderer = sdl.createRenderer(
         self.window, null,
         .{.accelerated = true},
-    ) catch try sdl.createRenderer(
-        self.window, null,
-        .{.software = true},
-    );
+    ) catch |err| fallback: {
+        std.debug.print("Error '{}' while using hardware accelarated rendering, falling back to software rendering", .{err});
+        break :fallback try sdl.createRenderer(
+            self.window, null,
+            .{.software = true},
+        );
+    };
     errdefer self.renderer.destroy();
 
     return self;
